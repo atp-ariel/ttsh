2b2f04571319f7f084d3a9271912417f92561be6
ArielTriana <usich37@gmail.com>
2020-10-13 16:29:16 -0400

    Adding helper and some comment

--- main.h
+++ main.h
@@ -10,24 +10,26 @@
 #include <sys/stat.h>
 #include "list.h"
 #include "process.h"
+#include "help.h"
+
 
 #pragma region DEFINITIONS
+/* Color definitions */
 #define COLOR_BLUE "\x1b[034m"
 #define COLOR_NONE "\x1b[0m"
-#define BOLD_TEXT "\033[1m"
-#define PROMPT "$ "
 #define COLOR_RED "\x1b[031m"
 #define COLOR_GREEN "\x1b[032m"
+/* End color definitions */
 
-#define MAXLETTERS 1000 // numero maximo de letras soportadas
-#define MAXCOMMANDS 100 // numero maximo de componentes soportados
-#define RL_BUFSIZE 1024
-#define NR_JOBS 512
-#define TOKEN_BUFSIZE 64
-#define TOKEN_DELIMITERS " \t\r\n\a\""
+/* Type of text definitions */
+#define BOLD_TEXT "\033[1m"
+/* End type of text definitions */
 
-#define HISTORY_LIMIT 10
+/* Prompt definitions */
+#define PROMPT "$ "
+/* End prompt definitions */
 
+/* Type of command definitions */
 #define COMMAND_EXTERN 0
 #define COMMAND_HISTORY 1
 #define COMMAND_AGAIN 2
@@ -36,7 +38,26 @@
 #define COMMAND_FG 5
 #define COMMAND_CD 6
 #define COMMAND_HELP 7
+/* End type of command definitions */
 
+/* History settings */
+#define HISTORY_LIMIT 10
+/* End history settings */
+
+/* General settings and variables*/
+#define MAXLETTERS 1000 // numero maximo de letras soportadas
+#define MAXCOMMANDS 100 // numero maximo de componentes soportados
+#define RL_BUFSIZE 1024
+#define NR_JOBS 512
+#define TOKEN_BUFSIZE 64
+#define TOKEN_DELIMITERS " \t\r\n\a\""
+
+struct info_shell *shell;
+list* history;
+pid_t  childpid;
+/* End general settings and variables*/
+
+/* Process status & execution */
 #define STATUS_RUNNING 0
 #define STATUS_DONE 1
 #define STATUS_SUSPENDED 2
@@ -47,30 +68,36 @@
 #define FOREGROUND_EXECUTION 1
 #define PIPELINE_EXECUTION 2
 
+const char* STATUS_STRING[] = {"Running", "Done", "Suspended", "Continued", "Terminated"};
+/* End process status & execution */
+
+/* Filters to process */
 #define FILTER_ALL 0
 #define FILTER_DONE 1
 #define FILTER_REMAINING 2
+/* End filters to process */
 
-const char* STATUS_STRING[] = {"Running", "Done", "Suspended", "Continued", "Terminated"};
-
+#pragma region InfoShell
 struct info_shell{
+    //Direction of the shell
     char cur_dir[MAXLETTERS];
+    //Jobs of in the shell
     struct job* jobs[NR_JOBS + 1];
     list* back_id;
 };
-
-struct info_shell *shell;
-list* history;
-pid_t  childpid;
-
+#pragma endregion InfoShell
 #pragma endregion DEFINITIONS
 
 #pragma region PROTOTYPES
-//Inicializa el SHELL
+// Shell initializate
 void shell_init();
-//Actualiza la dirección del directorio del SHELL
+
+// Update the direction of the shell on shell info
 void update_dir_info();
+
+// Return the direction of the user
 char* get_user_dir();
+
 //Imprime el Prompt del SHELL
 void print_prompt();
 //Ejecuta el proceso del SHELL
@@ -129,7 +156,7 @@ int get_proc_count(int id, int filter);
 void SIG_TRY_KILL_PROC(int signal);
 #pragma endregion PROTOTYPES
 
-#pragma region BUILTIN
+#pragma region BUILTIN.
 void shell_exit();
 int shell_cd(int argc, char** argv );
 int shell_history(struct job* job, struct process* proc, int in_fd, int out_fd, int mode);
@@ -139,17 +166,4 @@ int shell_fg(int argc, char** argv);
 void shell_help(struct job* job, struct process* proc, int in_fd, int out_fd, int mode);
 #pragma endregion BUILTIN
 
-#pragma region HELP
-#define  WELCOME "\033[1m\x1b[031mBienvenidos a la ayuda de \'ttsh\'\x1b[0m"
-#define  DEVELOPERS "\033[1m\x1b[031mIntegrantes:\x1b[0m Carlos Toledo Silva C-211\n\t     Ariel Alfonso Triana Pérez C-211"
-#define  FUNCTIONS "\033[1m\x1b[031mFuncionalidades:\n\x1b[032mbasic:\x1b[0m funcionalidades básicas (3 puntos)\n\033[1m\x1b[032mmulti-pipe:\x1b[0m múltiples tuberías (1 punto)\n\033[1m\x1b[032mbackground:\x1b[0m procesos ejecutándose en segundo plano (0.5 puntos)\n\033[1m\x1b[032mspaces:\x1b[0m comandos con cualquier cantidad de espacios (0.5 puntos)\n\033[1m\x1b[032mhistory:\x1b[0m historial de comandos (0.5 puntos)\n\033[1m\x1b[032mctrl+c:\x1b[0m envia la señal SIGINT al proceso que se ejecuta (0.5 puntos)\n\033[1m\x1b[032mhelp:\x1b[0m descripción de las funcionalidades del shell (1 punto)\n"
-#define  BUILTINS  "\033[1m\x1b[031mComandos built-in:\n\x1b[032mcd:\x1b[0m cambia de direcciones\n\033[1m\x1b[032mexit\x1b[0m termina el shell\n\033[1m\x1b[032mhistory:\x1b[0m muestra el historial\n\033[1m\x1b[032mfg:\x1b[0m devuelve al foreground un proceso del background\n\033[1m\x1b[032magain:\x1b[0m ejecuta un comando del historial\n\033[1m\x1b[032mhelp:\x1b[0m muestra la ayuda\n\033[1m\x1b[032mjobs:\x1b[0m muestra todos los jobs en cola\n\nPara ver la descripción de todos estos built-in ejecute el comando \033[1mhelp --all\x1b[0m"
-#define TOTAL_POINTS "\033[1m\x1b[031mTotal:\x1b[0m\t 7 puntos\n"
 
-#define EXIT_HELP "En el shell con el comando \033[1m\x1b[032mexit\x1b[0m se puede cerrar el mismo de forma que salve los comandos en el historial de comandos. Antes de cerrar el shell se mostrará un mensaje de despedida." 
-#define HISTORY_HELP "En el shell con el comando \033[1m\x1b[032mhistory\x1b[0m se puede consultar los últimos 10 comandos en las ejecuciones del shell.\n\nPara implementarlo se crea un archivo en el directorio /home/USER llamado \033[1mhistory.dat\x1b[0m donde se guardan los últimos 10 comandos. Es posible mostrar más de 10 comandos, solo es necesario definir la cantidad en la macro \033[1mHISTORY_LIMITS\x1b[0m del archivo \033[1mmain.h\x1b[0m. Cuando se inicializa el shell, se carga el historial en una lista, y al cerrar el shell se guarda la lista en el archivo. Si se desea que un comando no se guarde en el historial solo es necesario introducir un espacio antes del comando. Por ejemplo:\n\033[1mls\n echo hola mundo\x1b[0m\nEl comando \033[1mls\x1b[0m se guarda en el historial pero \033[1mecho hola mundo\x1b[0m no se guarda."
-#define AGAIN_HELP "En el shell con el comando \033[1m\x1b[032magain <index>\x1b[0m se puede volver a ejecutar el comando que ocupa la posición \033[1mindex\x1b[0m en el historial. Al ejecutar el comando lo que se guarda en el historial es el comando ha ejecutar por el \033[1magain\x1b[0m.\n\nEl comando \033[1magain\x1b[0m puede dar varios errores en ejecución:\n\t1. El primer error es que no exista ningún comando en el índice especificado y reportando el error: \'\033[1mUnexistent command in history at index: <index>\x1b[0m\'\n\t2. El segundo es que el índice esté fuera de la cantidad máxima de comandos en el historial, reportando el error: \'\033[1mIndex out of range of history\x1b[0m\'"
-#define CD_HELP "En el shell con el comando \033[1m\x1b[032mcd <dir>\x1b[0m se puede cambiar la dirección del shell.\n\nSi el comando \033[1mcd\x1b[0m no recibe ningún parámetro entonces cambia la dirección hacia \033[1m/home/USER\x1b[0m. Si recibe \033[1m<dir> = ..\x1b[0m cambia la dirección hacia el padre del actual directorio.\n\nAlguno de los problemas es que no se puede pasar como parámetro una dirección que contenga espacios, por ejemplo: \033[1m/home/USER/proyecto shell\x1b[0m."
-#define JOBS_HELP "En el shell con el comando  \033[1m\x1b[032mjobs\x1b[0m se puede obtener todos los comandos que se estan ejecutando en el background. Imprime por consola todos los procesos con el formato:\n \033[1m[<index>] <pid> <command> <state>\x1b[0m"
-#define FG_HELP "En el shell con el comando \033[1m\x1b[032mfg <index>\x1b[0m se puede traer un proceso de background hacia el foreground. Si no recibe ningún \033[1m<index>\x1b[0m el comando \033[1mfg\x1b[0m trae a foreground el último proceso que se envió a background. Si recibe algún \033[1mindex\x1b[0m envía a foreground el comando que está en ese índice en jobs."
-#pragma endregion HELP
